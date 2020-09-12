6c63088978ff672fba4cadbe1c5f3418cfe99e33
Frances Oparaocha <francesoparaocha@gmail.com>
2020-09-13 00:22:29 +0100

    changes

--- src/Components/LanguageSelector/index.tsx
+++ src/Components/LanguageSelector/index.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import './style.css';
-import { connect, Dispatch } from 'react-redux';
-import { changeLanguage } from 'Redux/Modules/Status';
-import { getTranslation, SupportedLanguage } from 'Services/Geo';
-import { RootState } from 'Redux/Store';
+import * as React from "react";
+import "./style.css";
+import { connect, Dispatch } from "react-redux";
+import { changeLanguage } from "Redux/Modules/Status";
+import { getTranslation, SupportedLanguage } from "Services/Geo";
+import { RootState } from "Redux/Store";
 
 const mapStateToProps = (state: RootState) => ({
-  lang: state.status.lang
+  lang: state.status.lang,
 });
 const mapDispatchToProps = (dispatch: Dispatch<{}>) => ({
   changeLanguage: (lang: SupportedLanguage) => {
     dispatch(changeLanguage(lang));
-  }
+  },
 });
 interface LanguageSelectorProps {
   changeLanguage: (lang: SupportedLanguage) => void;
@@ -22,23 +22,27 @@ class LanguageSelector extends React.Component<LanguageSelectorProps, {}> {
     if (this.props.lang !== lang) {
       this.props.changeLanguage(lang);
     }
-  }
+  };
   render() {
     return (
       <div className="languageSelector">
-        <span className="langText">{getTranslation(this.props.lang, 'Language')}: </span>
+        <span className="langText">{getTranslation(this.props.lang, "")} </span>
         <ul>
           <li
-            className={this.props.lang === SupportedLanguage.en ? ' active' : ''}
+            className={
+              this.props.lang === SupportedLanguage.en ? " active" : ""
+            }
             onClick={(e) => this.changeLanguage(SupportedLanguage.en)}
           >
-            <span className="flag uk" />
+            {/* <span className="flag uk" /> */}
           </li>
           <li
-            className={this.props.lang === SupportedLanguage.vn ? ' active' : ''}
+            className={
+              this.props.lang === SupportedLanguage.vn ? " active" : ""
+            }
             onClick={(e) => this.changeLanguage(SupportedLanguage.vn)}
           >
-            <span className="flag vn" />
+            {/* <span className="flag vn" /> */}
           </li>
         </ul>
       </div>
